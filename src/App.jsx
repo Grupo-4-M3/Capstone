9d097e06db2c0b4f0cc67f55de40a7cff2888348
renan-martini <renan.martiniduarte@gmail.com>
2022-07-06 14:02:20 -0300

    fix: exclusão do componente temporario do app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import "./App.css";
-import FormComponent from "./components/Form";
 
 function App() {
   const testFunction = (e) => {
@@ -7,25 +6,7 @@ function App() {
     console.log("foi");
   };
 
-  return (
-    <div className="App">
-      <FormComponent onSubmitFunction={testFunction} title="Login">
-        <label htmlFor="email">E-mail:</label>
-        <input id="email" type="email" placeholder="Digite seu email aqui..." />
-
-        <label htmlFor="senha">Senha:</label>
-        <input id="senha" type="email" placeholder="Digite sua senha aqui..." />
-
-        <label htmlFor="senha">Senha:</label>
-        <input id="senha" type="email" placeholder="Digite sua senha aqui..." />
-
-        <label htmlFor="senha">Senha:</label>
-        <input id="senha" type="email" placeholder="Digite sua senha aqui..." />
-
-        <button>Enviar</button>
-      </FormComponent>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -22,3 +22,21 @@ function FormComponent({ onSubmitFunction, children, title }) {
 }
 
 export default FormComponent;
+
+/* 
+<FormComponent onSubmitFunction={testFunction} title="Login">
+        <label htmlFor="email">E-mail:</label>
+        <input id="email" type="email" placeholder="Digite seu email aqui..." />
+
+        <label htmlFor="senha">Senha:</label>
+        <input id="senha" type="email" placeholder="Digite sua senha aqui..." />
+
+        <label htmlFor="senha">Senha:</label>
+        <input id="senha" type="email" placeholder="Digite sua senha aqui..." />
+
+        <label htmlFor="senha">Senha:</label>
+        <input id="senha" type="email" placeholder="Digite sua senha aqui..." />
+
+        <button>Enviar</button>
+      </FormComponent>
+*/
