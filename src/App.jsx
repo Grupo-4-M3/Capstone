70a054ce8ed0b2075aa32353eae2956916b6ea3e
renan-martini <renan.martiniduarte@gmail.com>
2022-07-06 13:52:22 -0300

    feat: estilização do componente form concluída

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,16 @@ function App() {
     <div className="App">
       <FormComponent onSubmitFunction={testFunction} title="Login">
         <label htmlFor="email">E-mail:</label>
-        <input id="email" type="email" />
+        <input id="email" type="email" placeholder="Digite seu email aqui..." />
 
         <label htmlFor="senha">Senha:</label>
-        <input id="senha" type="email" />
+        <input id="senha" type="email" placeholder="Digite sua senha aqui..." />
+
+        <label htmlFor="senha">Senha:</label>
+        <input id="senha" type="email" placeholder="Digite sua senha aqui..." />
+
+        <label htmlFor="senha">Senha:</label>
+        <input id="senha" type="email" placeholder="Digite sua senha aqui..." />
 
         <button>Enviar</button>
       </FormComponent>
--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,12 +1,21 @@
 import { StyledForm } from "./style";
 
-function FormComponent({ type, onSubmitFunction, children, title }) {
+function FormComponent({ onSubmitFunction, children, title }) {
   return (
     <StyledForm>
       <h2>{title}</h2>
 
       <form action="" onSubmit={onSubmitFunction}>
-        {children}
+        {children.map((child, index, array) => {
+          return index % 2 === 0 ? (
+            <div key={index}>
+              {child}
+              {array[index + 1]}
+            </div>
+          ) : (
+            <div className="ignore" key={index}></div>
+          );
+        })}
       </form>
     </StyledForm>
   );
