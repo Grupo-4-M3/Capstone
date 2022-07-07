037bd1742b14a508d4e7f65185c6a3584006b7d9
renan-martini <renan.martiniduarte@gmail.com>
2022-07-07 14:39:58 -0300

    fix: correção do componente form

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,14 @@
+import FormComponent from "../../components/Form";
+
 function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <FormComponent>
+        <label htmlFor="">nome:</label>
+        <input type="text" />
+      </FormComponent>
+    </main>
+  );
 }
 
 export default Home;
