f117c3db91f791835c0c8aaee47c8ee175c1f153
AlexFlavio <alexflavio.dev@gmail.com>
2022-07-07 11:01:44 -0300

    fix: responsividade adicionada, dados dispostos de forma mais automatica, adicionado propriedades no componente tanto para os dados do usuario quanto props internos para personalização da altura do header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,18 +1,79 @@
+
 import { useHistory } from "react-router-dom";
 import { StyledHeader } from "./styles";
+import {Button} from "./../Button"
 
-export function Header({children}){
-
+export function Header({children,user,type}){
     const historico = useHistory()
 
     const levarAoDashboard = ()=>{
         historico.push("./dashboard")
     }
 
-    return(
-        <StyledHeader> 
-            <h1 onClick={levarAoDashboard}>Call<span>Mind</span></h1>
-              <div>{children}</div>
-              </StyledHeader>
-    )
+    const levarAoHome = ()=>{
+        historico.push("./home")
+    }
+    const levarAoLogin = ()=>{
+        historico.push()
+    }
+    const levarAoRegistro = ()=>{
+        historico.push()
+    }
+    
+    switch (type) {
+        case "home":
+            return (
+                <StyledHeader>
+                     <h1>Call<span>Mind</span></h1>
+                     <div>
+                        <Button onclick={levarAoLogin} nameButton={"Login"}></Button> 
+
+                        <Button onclick={levarAoRegistro} nameButton={"Registro"}>Registro</Button>
+                     </div>
+                </StyledHeader>
+            )
+        case "login":
+            return(
+                <StyledHeader>
+                    <h1 onClick={levarAoHome}>Call<span>Mind</span></h1>
+                    <div><Button onclick={levarAoRegistro} nameButton={"Registro"}></Button></div>
+                </StyledHeader>
+            )
+        case "registro":
+                return(
+                    <StyledHeader>
+                        <h1 onClick={levarAoHome}>Call<span>Mind</span></h1>
+                        <div><Button onclick={levarAoLogin} nameButton={"Login"}></Button></div>
+                    </StyledHeader>
+                )
+        case "cadastro":
+            return(
+                <StyledHeader>
+                    <h1>Call<span>Mind</span></h1>
+                    <div><Button onclick={levarAoHome} nameButton={"Logout"}></Button></div>
+                </StyledHeader>
+            )
+        case "dashBoard":
+            
+             return(
+                <StyledHeader dash>
+                    <h1 onClick={levarAoDashboard}>Call<span>Mind</span></h1>
+                    <div>
+                        <figure>
+                            <img src={!!user ? user.img : ""} alt="" />
+                        </figure>
+                        <div>
+                            <h2>{!!user? user.name: "Teste"}</h2>
+                            <Button nameButton={"Logout"}></Button>
+                        </div>
+                    </div>   
+                </StyledHeader>
+             )
+        default:
+            <StyledHeader> 
+                <h1 onClick={levarAoDashboard}>Call<span>Mind</span></h1>
+                <div>{children}</div>
+            </StyledHeader>
+        break;
+    }
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,17 +1,19 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+
 
 export const StyledHeader = styled.header`
     display: flex;
     justify-content: space-between;
     align-items: center;
     width: 100vw;
-    height: 60px;
+    height: ${props=> props.heigth || "70px"};
     background: #E9DAC1;
     border: 1px solid rgba(0, 0, 0, 0.49);
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     border-radius: 0px 0px 8px 8px;
     box-sizing: border-box;
-    padding: 0 3rem;
+    padding: 0 .5rem;
 
     & h1 {
         font-family: "Montserrat", inter;
@@ -21,5 +23,50 @@ export const StyledHeader = styled.header`
     }
     & h1 span{
         color: #448382 ;
+    }
+    & div{
+        display:flex;
+        flex-direction: column;
+        justify-content:flex-end;
+        align-items:center;
+        gap: .1rem;
+    }
+    & figure{
+        background-color: red;
+        width: 3rem;
+        height: 3rem;
+        border-radius: 50%;
+    }
+    ${props =>props.dash && css`
+       & div{
+            flex-direction: row;
+            gap: .5rem;
+        }
+        & div div {
+            flex-direction: column;
+            gap: .08rem
+        }
+    `}
+
+    @media (min-width: 350px){
+        & div{
+            flex-direction: row;
+            gap: 1rem;
+        }
+    }
+    @media (min-width: 600px){
+        padding: 0 2rem;
+
+        ${props=>props.dash && css`
+            & div{
+                gap: 0.3rem;
+                }
+            & div div {
+                
+                    flex-direction: row;
+                    gap: 2rem;
+                    }
+        `}
+        
     }
 `
