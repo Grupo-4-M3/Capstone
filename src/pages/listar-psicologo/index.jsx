02caaf211c02b9928ae6db835a9086204ae3e646
RodolfoJaques <rodolfojaques13@gmail.com>
2022-07-09 20:42:55 -0300

    feat: criação da pagina ListarPsicologo_ logica/29%

--- src/pages/listar-psicologo/index.jsx
+++ src/pages/listar-psicologo/index.jsx
@@ -1,27 +1,54 @@
+import { useContext, useEffect, useState } from "react";
 import Calendar from "react-calendar";
 import 'react-calendar/dist/Calendar.css';
+import { useParams } from "react-router-dom";
 
 import Card from "../../components/Card";
 import { Header } from "../../components/Header";
 import { SecMain } from "./styles";
+import { API } from "../../services/api";
 
 function ListarPsicologo() {
+
+  const [pessoa,setPessoa] = useState({})
+  const [usuario,setUsuario] = useState({})
+
+  const params = useParams()
+
+  const { id } = params 
+
+  const usuarioProvider = ''
+  const tokem = ''
+
+  useEffect(()=>{
+    
+    API.get(`/psychologists?userId=${id}`,{
+      headers: {
+          'Authorization': `Bearer ${tokem}`
+      }
+    })
+    .then((resp)=> setPessoa(resp.data[0]))
+    .catch((err)=> console.log(err))
+  },[])
+
+  useEffect(()=>{
+    
+    API.get(`/patients?userId=${usuarioProvider.userId}`,{
+      headers: {
+          'Authorization': `Bearer ${tokem}`
+      }
+    })
+    .then((resp)=> console.log(resp.data[0]))//setUsuario(resp.data[0]))
+    .catch((err)=> console.log(err))
+  },[])
+
   return <SecMain>
-    <Header type={"dashBoard"}/>
+    <Header type={"dashBoard"} user={usuario}/>
     <div className="alinhamento">
       <section className="container">
-        <Card type={"psicologo"} pessoa={{
-          "userId": 3,
-          "name": "Rogerio Alves Cardoso",
-          "img":"https://www.attendit.net/images/easyblog_shared/July_2018/7-4-18/totw_network_profile_400.jpg",
-          "emphasis": "Psicologia Clínica com ênfase em psicanalise",
-          "experience": "5 anos",
-          "schedules": "8:00 - 12:00",
-          "working_days": "Seg/Qua/Sex",
-          "patients": [],
-          "calendar": [],
-          "id": 1
-          }}
+        <Card 
+          type={"psicologo"} 
+          pessoa={pessoa}
           maxSizeY="70vh"
           />
       </section>
