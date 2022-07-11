3cc02b52916842f3bd888cd544eeb7b0988508fc
renan-martini <renan.martiniduarte@gmail.com>
2022-07-11 16:01:56 -0300

    feat: adição da função de atualização da agenda

--- src/components/modal_confirmacao/index.jsx
+++ src/components/modal_confirmacao/index.jsx
@@ -1,28 +1,51 @@
 import { Box, Modal } from "@mui/material";
 import { useGoogleLogin } from "@react-oauth/google";
 import axios from "axios";
-import { useContext } from "react";
 import { toast } from "react-toastify";
-import { UserContext } from "../../providers/user";
 import API from "../../services/api";
 import { Button } from "../Button";
 import { style } from "./style";
 
 function ModalConfirmation({
-  hora,
   dia,
-  paciente,
-  psicologo,
   open,
   setOpen,
   evento,
+  paciente,
+  psicologo,
 }) {
-  const { usuario } = useContext(UserContext);
   const updateCalendars = (res) => {
-    const calendarPaciente = paciente.calendar;
-    const calendarPsicologo = psicologo.calendar;
-    API.patch;
+    const newEvent = {
+      ...evento,
+      paciente: paciente,
+      disponivel: false,
+      link: res?.data?.hangoutLink,
+      id_reunião: res?.data?.id,
+    };
+    const newPacient = {
+      ...paciente,
+    };
+    const newPatients = () => {
+      const jaExiste = psicologo.patients.find(
+        (patient) => patient.id === paciente.id
+      );
+      if (jaExiste) {
+        return psicologo.patients;
+      }
+      return [...psicologo.patients, paciente];
+    };
+    const newPsicologo = {
+      ...psicologo,
+      patients: newPatients(),
+    };
+    newPacient.calendar[dia]["hora" + evento?.horario?.split(":")[0]] =
+      newEvent;
+    newPsicologo.calendar[dia]["hora" + evento?.horario?.split(":")[0]] =
+      newEvent;
+    API.patch(`/patients/${paciente.id}`, newPacient);
+    API.patch(`/psychologists/${psicologo.id}`, newPsicologo);
   };
+
   const googleResponse = (res) => {
     const { code } = res;
     axios
@@ -33,17 +56,19 @@ function ModalConfirmation({
             summary: `Consulta de ${paciente?.name} com ${psicologo?.name}`,
             description: `Queixas do paciente: ${paciente?.complaint}`,
             location: "Online",
-            startDateTime: hora,
-            endDateTime: hora,
+            startDateTime: evento.hora,
+            endDateTime: evento.hora,
             attendees: [{ email: paciente.email }, { email: psicologo.email }],
           })
           .then((res) => {
             toast.success("Evento Agendado com sucesso!");
+            updateCalendars(res);
           })
           .catch((error) => console.log(error));
       })
       .catch((error) => console.log(error));
   };
+
   const login = useGoogleLogin({
     onSuccess: (code) => {
       googleResponse(code);
@@ -52,10 +77,17 @@ function ModalConfirmation({
     flow: "auth-code",
     onError: (error) => console.log(error),
   });
+
   return (
     <Modal open={open} onClose={() => setOpen(false)}>
       <Box sx={style}>
-        <Button onclick={login} />
+        <Button
+          onclick={login}
+          nameButton="Confirmar"
+          backcolor="#54BAB9"
+          size="100px"
+          sizeY="50px"
+        />
       </Box>
     </Modal>
   );
